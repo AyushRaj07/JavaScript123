4c42aaa3eafe4d3cca30489d6e9898752fec220e
AyushRaj07 <74702706+AyushRaj07@users.noreply.github.com>
2024-10-28 11:22:13 +0000

    commit

--- .devcontainer/basics/test.js
+++ .devcontainer/basics/test.js
@@ -13,3 +13,37 @@ console.table([a,b,c,d])
 "use strict"; //treat all js code as newer version
 
 // console.log("abcd")
+
+let ab = "abcd"
+
+//number conversion
+let valNum = Number(ab)
+console.log(valNum);
+
+console.log();
+
+//"33" = 33
+//"33abc" = NaN
+//true = 1; false = 0;
+
+//Boolean conversion
+
+//1 = true; 0 = false
+//"" = false
+//"abcd"=true
+
+//operations
+
+let str1 = "abcd"
+let str2 = " efgh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2); //12 
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+
+
+
